86bb8f689b317825c2ab6de321e0fd9e779c525f
HollyC13 <hrcrtr@prodigy.net>
2021-10-31 19:22:24 -0500

    Updated files

--- scraping.py
+++ scraping.py
@@ -13,14 +13,14 @@ def scrape_all():
 
     news_title, news_paragraph = mars_news(browser)
 
-    # Run all scrapingfunctions and store results in a dictionary
+    # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-        "mars_hemispheres": hemispheres
+        "last_modified": dt.datetime.now(),
+        "hemispheres": mars_hemi(browser)
     }
 
     # Stop webdriver and return data
@@ -54,6 +54,7 @@ def mars_news(browser):
         return None, None
 
     return news_title, news_p
+
 
 # ## JPL Space Images Featured Image
 
@@ -106,19 +107,24 @@ def mars_facts():
     df.set_index('description', inplace=True)
 
     # Convert DataFrame back to HTML, add bootstrap
-    return df.to_html(classes="table table-striped")
+    return df.to_html(classes="table table-striped table-bordered")
 
 # Function to scrape the hemisphere data from Challenge file
-def hemi_scrape():
+def mars_hemi(browser):
+
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    
     # 2. Create a list to hold the images and titles.
     hemisphere_image_urls = []
 
         # 3. Write code to retrieve the image urls and titles for each hemisphere
-    for hemisphere in range(4):
+    for hemi in range(4):
         hemispheres = {}
     
         # Find element and use it to navigate from main page to pages holding full-resolution images
-        browser.find_by_css('h3')[hemisphere].click()
+        browser.find_by_css('h3')[hemi].click()
 
         # Parse the resulting html with soup
         html = browser.html
@@ -137,10 +143,8 @@ def hemi_scrape():
         # Navigate back to main page
         browser.back() 
 
-
-        # %%
-        # 4. Print the list that holds the dictionary of each image url and title.
-        hemisphere_image_urls
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 # Tell Flask script is complete and "ready for action", print scraping results to terminal
 if __name__ == "__main__":
